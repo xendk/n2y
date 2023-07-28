f002188fa81de024d3b6b56253e4280550c1a7d6
Thomas Fini Hansen <xen@xen.dk>
2023-07-28 11:35:41 +0200

    Fix version in shard.yml and make VERSION constant use it

--- src/n2y.cr
+++ src/n2y.cr
@@ -1,6 +1,6 @@
 # TODO: Write documentation for `N2y`
 module N2y
-  VERSION = "0.1.0"
+  VERSION = {{ `shards version "#{__DIR__}"`.chomp.stringify }}
 
   DEFAULT_DB_URL = "sqlite3://./db/n2y.db"
 end
