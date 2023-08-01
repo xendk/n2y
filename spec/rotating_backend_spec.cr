fb3c8fbdee3587d473f2fb7da88279baba0a8935
Thomas Fini Hansen <xen@xen.dk>
2023-08-01 21:43:23 +0200

    Create rotating log backend

---
+++ spec/rotating_backend_spec.cr
@@ -0,0 +1,85 @@
+require "./spec_helper"
+require "file_utils"
+require "../src/n2y/rotating_backend"
+
+include N2y
+
+def with_storage(&)
+  tmp_dir = "/tmp/n2y-test/storage/logs"
+  FileUtils.rm_rf tmp_dir
+  Dir.mkdir_p tmp_dir
+
+  RotatingBackend.settings.storage_path = "/tmp/n2y-test/storage/logs"
+
+  yield
+end
+
+describe RotatingBackend do
+  it "should write to user log file" do
+    with_storage do
+      Log.with_context do
+        Log.context.set user_id: "test"
+
+        entry = Log::Entry.new(
+          "here",
+          Log::Severity::Error,
+          "log message",
+          Log::Metadata.new(nil, {json: "{}"}),
+          nil,
+          timestamp: Time.utc(2023, 5, 17, 18, 35, 27)
+        )
+
+        RotatingBackend.new().write(entry)
+
+        entry = Log::Entry.new(
+          "here2",
+          Log::Severity::Error,
+          "second log message",
+          Log::Metadata.new(nil),
+          nil,
+          timestamp: Time.utc(2023, 5, 17, 18, 35, 35)
+        )
+
+        RotatingBackend.new().write(entry)
+
+        File.exists?("/tmp/n2y-test/storage/logs/test/2023-05-17.log").should eq true
+        File.read("/tmp/n2y-test/storage/logs/test/2023-05-17.log").should eq "2023-05-17T18:35:27.000000Z\ttest\tERROR\tlog message\t{}\n2023-05-17T18:35:35.000000Z\ttest\tERROR\tsecond log message\n"
+      end
+    end
+  end
+
+  it "should rotate log files" do
+    with_storage do
+      Log.with_context do
+        Log.context.set user_id: "test"
+
+        entry = Log::Entry.new(
+          "here",
+          Log::Severity::Error,
+          "log message",
+          Log::Metadata.new(nil, {json: "{}"}),
+          nil,
+          timestamp: Time.utc(2023, 5, 17, 18, 35, 27)
+        )
+
+        RotatingBackend.new().write(entry)
+
+        entry = Log::Entry.new(
+          "here2",
+          Log::Severity::Error,
+          "second log message",
+          Log::Metadata.new(nil),
+          nil,
+          timestamp: Time.utc(2023, 5, 18, 18, 35, 35)
+        )
+
+        RotatingBackend.new().write(entry)
+
+        File.exists?("/tmp/n2y-test/storage/logs/test/2023-05-17.log").should eq true
+        File.exists?("/tmp/n2y-test/storage/logs/test/2023-05-18.log").should eq true
+        File.read("/tmp/n2y-test/storage/logs/test/2023-05-17.log").should eq "2023-05-17T18:35:27.000000Z\ttest\tERROR\tlog message\t{}\n"
+        File.read("/tmp/n2y-test/storage/logs/test/2023-05-18.log").should eq "2023-05-18T18:35:35.000000Z\ttest\tERROR\tsecond log message\n"
+      end
+    end
+  end
+end
---
+++ src/n2y/rotating_backend.cr
@@ -0,0 +1,73 @@
+require "log"
+require "habitat"
+require "log/json"
+require "file_utils"
+
+module N2y
+  class RotatingBackend < Log::Backend
+    Habitat.create do
+      setting storage_path : String
+    end
+
+    class TabFormat
+      extend Log::Formatter
+
+      def self.format(entry : Log::Entry, io : IO)
+        entry.timestamp.to_rfc3339(io, fraction_digits: 6)
+        io << "\t"
+        io << entry.context[:user_id].as_s
+        io << "\t"
+        io << entry.severity.label
+        io << "\t"
+        io << entry.message.gsub('\t', ' ')
+        if entry.data[:json]?
+          io << "\t"
+          io << entry.data[:json].as_s
+        end
+      end
+    end
+
+    @formatter = TabFormat
+    @daystamp : Int32?
+    @files = {} of String => IO
+
+    def initialize()
+      @mutex = Mutex.new(:unchecked)
+    end
+
+    def write(entry : Log::Entry)
+      @mutex.synchronize do
+        daystamp = (entry.timestamp.year * 10000) +
+                   (entry.timestamp.month * 100) +
+                   (entry.timestamp.day)
+
+        # New day, close open files.
+        close if daystamp != @daystamp
+
+        user_id = entry.context[:user_id].as_s
+
+        if @files[user_id]?.nil?
+          directory = File.join(settings.storage_path, user_id)
+          Dir.mkdir_p directory
+          filename = "%04d-%02d-%02d.log" % entry.timestamp.try { |d| [d.year, d.month, d.day] }
+          @files[user_id] = File.open(File.join(directory, filename), "a")
+        end
+
+        format(entry, @files[user_id])
+        @files[user_id].puts
+        @files[user_id].flush
+      end
+    end
+
+    # Emits the *entry* to the given *io*.
+    # It uses the `#formatter` to convert.
+    def format(entry : Log::Entry, io : IO)
+      @formatter.format(entry, io)
+    end
+
+    def close
+      @files.each_value { |io| io.close }
+      @files = {} of String => IO
+    end
+  end
+end
