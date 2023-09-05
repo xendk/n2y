0adf50022a7f15e747504e2116fcfd8ac6eef137
Thomas Fini Hansen <xen@xen.dk>
2023-09-05 22:16:00 +0200

    Simplify YNAB::Transaction initialize
    And remove unused `valid?` method.

--- src/n2y/ynab/transaction.cr
+++ src/n2y/ynab/transaction.cr
@@ -14,16 +14,7 @@ module N2y
 
       def_equals :account_id, :date, :amount, :payee_name, :import_id
 
-      def initialize(*, account_id : String? = nil, date : String? = nil, amount : Int32? = nil, payee_name : String? = nil, import_id : String? = nil)
-        @account_id = account_id
-        @date = date
-        @amount = amount
-        @payee_name = payee_name
-        @import_id = import_id
-      end
-
-      def valid?
-        @account_id && @date && @amount && @payee_name && @import_id
+      def initialize(*, @account_id : String? = nil, @date : String? = nil, @amount : Int32? = nil, @payee_name : String? = nil, @import_id : String? = nil)
       end
     end
   end
